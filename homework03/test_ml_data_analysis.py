ac2ed2f93161d7c0e7a972d69397ffee9730dfc6
Hieu Nguyen <hieu.nguyen@hyneskhater.com>
2024-02-13 00:55:57 -0600

    update unit tests

--- homework03/test_ml_data_analysis.py
+++ homework03/test_ml_data_analysis.py
@@ -24,12 +24,11 @@ def test_date_distribution():
           == {'Before 1900': 1, '1900-1925': 1, '1926-1950': 1, '1951-1975': 1, '1976-2000': 1, '2001-2010': 1, 'After 2010': 0}
     assert date_distribution([]) == {'Before 1900': 0, '1900-1925': 0, '1926-1950': 0, '1951-1975': 0, '1976-2000': 0, '2001-2010': 0, 'After 2010': 0}
 
-def location_mass_distribution():
-    assert location_mass_distribution([{'reclat': '40.7128', 'reclong': '-74.0060', 'mass (g)': '1000'},
+def test_location_mass_distribution():
+    assert location_mass_distribution([{'reclat': '40.7128', 'reclong': '74.0060', 'mass (g)': '1000'},
          {'reclat': '34.0522', 'reclong': '-118.2437', 'mass (g)': '2000'},
-         {'reclat': '', 'reclong': '-118.2437', 'mass (g)': '500'},
-         {'reclat': '37.7749', 'reclong': '', 'mass (g)': '1500'},
-         {'reclat': '41.8781', 'reclong': '-87.6298', 'mass (g)': '800'}]) == \
+         {'reclat': '-37.7749', 'reclong': '87.6298', 'mass (g)': '1500'},
+         {'reclat': '-41.8781', 'reclong': '-87.6298', 'mass (g)': '800'}]) == \
          {
             'Northern & Eastern': 1000.0,
             'Northern & Western': 2000.0,
