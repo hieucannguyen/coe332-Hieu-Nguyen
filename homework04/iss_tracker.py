c0eb299059009ea672de34f174af923d1ef7fafb
Hieu Nguyen <hieu.nguyen@hyneskhater.com>
2024-02-19 22:07:23 -0600

    update logging

--- homework04/iss_tracker.py
+++ homework04/iss_tracker.py
@@ -14,8 +14,6 @@ import socket
 parser = argparse.ArgumentParser()
 parser.add_argument('-l', '--loglevel', type=str, required=False, default='WARNING',
                     help='set log level to DEBUG, INFO, WARNING, ERROR, or CRITICAL')
-parser.add_argument('-f', '--filename', type=str, required=False, default='Meteorite_Landings.csv',
-                    help='set to file name of the meteorite landings data set')
 args = parser.parse_args()
 
 format_str=f'[%(asctime)s {socket.gethostname()}] %(filename)s:%(funcName)s:%(lineno)s - %(levelname)s: %(message)s'
