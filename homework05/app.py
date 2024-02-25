8bf68b77f81dda720c816028f0b344d1ce76c828
Hieu Nguyen <hieu.nguyen@hyneskhater.com>
2024-02-25 17:58:02 -0600

    first commit

---
+++ homework05/app.py
@@ -0,0 +1,62 @@
+from flask import Flask, request
+from iss_tracker import get_data, find_closest_epoch, compute_speed
+
+app = Flask(__name__)
+
+
+@app.route("/epochs", methods=["GET"])
+def get_epochs():
+    data = get_data()
+    limit = int(request.args.get("limit", 0))
+    offset = int(request.args.get("offset", 0))
+    if limit and offset:
+        result = []
+        for i in range(limit):
+            result.append(data[offset + i])
+        return result
+
+    return data
+
+
+@app.route("/epochs/<epoch>", methods=["GET"])
+def get_specific_epoch(epoch):
+    data = get_data()
+    for item in data:
+        if item["EPOCH"] == epoch:
+            return item
+
+    return "Epoch not found."
+
+
+@app.route("/epochs/<epoch>/speed", methods=["GET"])
+def get_specific_epoch_speed(epoch):
+    data = get_data()
+    for item in data:
+        if item["EPOCH"] == epoch:
+            return {
+                "EPOCH": item["EPOCH"],
+                "Speed (km/s)": compute_speed(
+                    float(item["X_DOT"]["#text"]),
+                    float(item["Y_DOT"]["#text"]),
+                    float(item["Z_DOT"]["#text"]),
+                ),
+            }
+
+    return "Epoch not found."
+
+
+@app.route("/now", methods=["GET"])
+def get_current_epoch():
+    data = get_data()
+    current_epoch = find_closest_epoch(data)
+    current_epoch["Speed (km/s)"] = compute_speed(
+        float(current_epoch["X_DOT"]["#text"]),
+        float(current_epoch["Y_DOT"]["#text"]),
+        float(current_epoch["Z_DOT"]["#text"]),
+    )
+
+    return current_epoch
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
