0d6362959a98d5dd356ff7a5c4d7b473733b700f
hieucannguyen <hieucanguyen@yahoo.com>
2024-04-15 03:30:00 +0000

    fix pytests

--- homework08/test/test_gene_api.py
+++ homework08/test/test_gene_api.py
@@ -1,10 +1,12 @@
 import pytest
 import requests
 import time
+import json
 
 def test_handle_data_DELETE():
     response = requests.delete('http://127.0.0.1:5000/data')
     assert response.status_code == 200
+    assert response.json()['message'] == 'Data deleted successfully'
     assert isinstance(response.json(), dict) == True
     
 def test_handle_data_POST():
@@ -18,18 +20,29 @@ def test_handle_data_GET():
     assert response.status_code == 200
     assert isinstance(response.json(), list) == True
 
-JOB_ID = ''
-
 def test_submit_jobs():
+    data = {"gene_group": "Antisense RNAs"}
+    # Convert data to JSON format
+    json_data = json.dumps(data)
     response = requests.post("http://127.0.0.1:5000/jobs", 
-    data={"gene_group":"Antisense RNAs"},
-    headers={"Content-Type": "application/json"},
+    data=json_data,
+    headers={"Content-Type": "application/json"}
     )
-    print(response.json())
     JOB_ID = response.json()['id']
-    print(JOB_ID)
     assert response.status_code == 200
     assert response.json()['id'] == JOB_ID
+    assert isinstance(response.json(), dict) == True
+
+    # jobs GET test
+    response = requests.get('http://127.0.0.1:5000/jobs/'+JOB_ID)
+    assert response.status_code == 200
+    assert response.json()['id'] == JOB_ID
+    assert isinstance(response.json(), dict) == True
+    
+    # jobs result GET test
+    time.sleep(8)
+    response = requests.get('http://127.0.0.1:5000/results/'+JOB_ID)
+    assert response.status_code == 200
     assert isinstance(response.json(), dict) == True
 
 def test_get_jobs():
@@ -37,19 +50,6 @@ def test_get_jobs():
     assert response.status_code == 200
     assert isinstance(response.json(), list) == True
 
-def test_get_job():
-    response = requests.get('http://127.0.0.1:5000/jobs/'+JOB_ID)
-    assert response.status_code == 200
-    assert response.json()['id'] == JOB_ID
-    assert isinstance(response.json(), dict) == True
-
-def test_get_result():
-    time.sleep(10)
-    response = requests.get('http://127.0.0.1:5000//results'+JOB_ID)
-    assert response.status_code == 200
-    assert response.json()['message'] == 'Data deleted successfully'
-    assert isinstance(response.json(), dict) == True
-    
 def test_get_genes():
     response = requests.get('http://127.0.0.1:5000/genes')
     assert response.status_code == 200
