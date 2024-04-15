0e6ed0b08d9473b9ebe85dfaa90e532579d5e78c
Hieu Nguyen <hieu.nguyen@hyneskhater.com>
2024-04-14 21:49:14 -0500

    update tests

--- homework08/test/test_gene_api.py
+++ homework08/test/test_gene_api.py
@@ -10,6 +10,7 @@ def test_submit_jobs():
     headers={"Content-Type": "application/json"},
     )
     JOB_ID = response.json()['id']
+    print(JOB_ID)
     assert response.status_code == 200
     assert response.json()['id'] == JOB_ID
     assert isinstance(response.json(), dict) == True
@@ -26,10 +27,10 @@ def test_get_job():
     assert isinstance(response.json(), dict) == True
 
 def test_get_result():
-    time.sleep(15)
+    time.sleep(10)
     response = requests.get(f'http://127.0.0.1:5000//results/{JOB_ID}')
     assert response.status_code == 200
-    assert response.json()['messsage'] == 'Data deleted successfully'
+    assert response.json()['message'] == 'Data deleted successfully'
     assert isinstance(response.json(), dict) == True
 
 def test_handle_data_DELETE():
@@ -40,7 +41,7 @@ def test_handle_data_DELETE():
 def test_handle_data_POST():
     response = requests.post("http://127.0.0.1:5000/data")
     assert response.status_code == 200
-    assert response.json()['messsage'] == 'Data added successfully'
+    assert response.json()['message'] == 'Data added successfully'
     assert isinstance(response.json(), dict) == True
 
 def test_handle_data_GET():
@@ -57,4 +58,4 @@ def test_get_specific_gene():
     response = requests.get('http://127.0.0.1:5000/genes/HGNC:20488')
     assert response.status_code == 200
     assert response.json()['hgnc_id'] == 'HGNC:20488'
-    assert response(response.json(), dict) == True
+    assert isinstance(response.json(), dict) == True
